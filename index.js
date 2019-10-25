63fce47f17707a2622b196b969a1bc35733ce291
shuaibo.yao <15810856489@163.com>
2019-10-25 17:42:24 +0800

    将主程序修改为 async await

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-const program = require("commander");
-const child = require("child_process");
 const start = require("./server.js");
 const detect = require("detect-port");
 const inquirer = require('react-dev-utils/inquirer');
@@ -10,7 +8,6 @@ const question = require("./script/question.js");
 const useTs = require("./script/useTs.js");
 const DEFAULT_PORT = question.DEFAULT_PORT; // 默认端口号
 // 初始化执行函数
-// {port, entryFile, canUseTs}
 async function init(entryFile) {
     try {
         const {canUseTs} = await useTs();
@@ -23,7 +20,7 @@ async function init(entryFile) {
         const _port = await detect(DEFAULT_PORT);
         if ( DEFAULT_PORT === _port ) {
             start(startParam);
-            return ;
+            return false;
         } else {
             try {
                 const { changePort } =  await inquirer.prompt([question.postQuestion]);
